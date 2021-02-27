cd22f548ff59ef9378b8a800867e6ce42cfe26ff
siriwatknp <siriwatkunaporn@gmail.com>
2021-02-27 22:49:46 +0700

    feat: 🎸 configure tailwind for build

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,7 +1,30 @@
 import React, { PropsWithChildren } from 'react';
+import cx from 'clsx';
 
-export const Button = ({ children }: PropsWithChildren<{}>) => (
-  <button className="px-2 py-1 outline-none bg-blue-500 color-white rounded">
-    {children}
-  </button>
-);
+export type ButtonProps = {
+  primary?: boolean;
+} & JSX.IntrinsicElements['button'];
+
+export const Button = ({
+  children,
+  primary,
+  className,
+  ...props
+}: PropsWithChildren<ButtonProps>) => {
+  const baseClassName =
+    'bg-gray-100 text-gray-900 hover:bg-gray-300 focus:ring-gray-400';
+  const primaryClassName =
+    'bg-green-500 text-white hover:bg-green-700 focus:ring-green-400';
+  return (
+    <button
+      className={cx(
+        `py-2 px-4 font-semibold rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-opacity-75`,
+        primary ? primaryClassName : baseClassName,
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
